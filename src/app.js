445214097d87f17c1d13b38746e58b98a2ba8df0
thenesterov <thenesterov@proton.me>
2023-01-02 01:11:17 +0500

    Add movable shapes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 var Colors = (function () {
     function Colors() {
     }
-    Colors.MAIN = "#65bbf4";
+    Colors.MAIN = '#65bbf4';
     return Colors;
 }());
+var allShapes = [];
+var mouseIsDown = false;
+var diffMouseX = 0;
+var diffMouseY = 0;
+var selectedShape;
 var Canvas = (function () {
     function Canvas() {
         var canvas = document.getElementById('canvas');
@@ -13,10 +18,70 @@ var Canvas = (function () {
         this.canvas = canvas;
         this.context = context;
     }
-    Canvas.prototype.drawShape = function (shape, backgroundColor) {
-        this.context.fillStyle = String(backgroundColor);
-        this.context.fill(shape);
+    Canvas.prototype.addShape = function (shape) {
+        allShapes.push(shape);
+    };
+    Canvas.prototype.drawShape = function (shape) {
+        if (shape instanceof Rect) {
+            this.context.fillStyle = String(shape.color);
+            this.context.fillRect(shape.posX, shape.posY, shape.width, shape.height);
+        }
     };
     return Canvas;
 }());
+var Shape = (function () {
+    function Shape() {
+    }
+    return Shape;
+}());
+var Rect = (function () {
+    function Rect(posX, posY, width, height, color) {
+        this.width = 100;
+        this.posY = 100;
+        this.posX = 100;
+        this.height = 100;
+        this.color = Colors.MAIN;
+        this.movable = true;
+        this.clickdownable = true;
+        this.dblclickable = true;
+        this.path2d = new Path2D;
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.path2d.rect(this.posX, this.posY, this.width, this.height);
+    }
+    return Rect;
+}());
 var canvas = new Canvas();
+canvas.canvas.addEventListener('mouseup', function (ev) {
+    mouseIsDown = false;
+    selectedShape = null;
+});
+canvas.canvas.addEventListener('mousedown', function (ev) {
+    allShapes.forEach(function (shape) {
+        if (canvas.context.isPointInPath(shape.path2d, ev.offsetX, ev.offsetY) && shape.clickdownable) {
+            mouseIsDown = true;
+            selectedShape = shape;
+            diffMouseX = ev.offsetX - shape.posX;
+            diffMouseY = ev.offsetY - shape.posY;
+        }
+    });
+});
+canvas.canvas.addEventListener('mousemove', function (ev) {
+    if (mouseIsDown) {
+        allShapes[allShapes.indexOf(selectedShape)].posX = ev.offsetX - diffMouseX;
+        allShapes[allShapes.indexOf(selectedShape)].posY = ev.offsetY - diffMouseY;
+        allShapes[allShapes.indexOf(selectedShape)].path2d = new Path2D();
+        allShapes[allShapes.indexOf(selectedShape)].path2d.rect(allShapes[allShapes.indexOf(selectedShape)].posX, allShapes[allShapes.indexOf(selectedShape)].posY, allShapes[allShapes.indexOf(selectedShape)].width, allShapes[allShapes.indexOf(selectedShape)].height);
+    }
+});
+function renderCanvas() {
+    canvas.context.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
+    allShapes.forEach(function (shape) {
+        canvas.drawShape(shape);
+    });
+    window.requestAnimationFrame(renderCanvas);
+}
+window.requestAnimationFrame(renderCanvas);
--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,19 @@ type HEX = `#${string}`;
 type Color = RGB | RGBA | HEX;
 
 abstract class Colors {
-    static MAIN: Color = "#65bbf4";
+    static MAIN: Color = '#65bbf4';
 }
 
+let allShapes: Shape[] = [];
+
+let mouseIsDown: boolean = false;
+let diffMouseX: number = 0;
+let diffMouseY: number = 0;
+
+let selectedShape: Shape | null;
+
 class Canvas {
-    private canvas: HTMLCanvasElement;
+    public canvas: HTMLCanvasElement;
     public context: CanvasRenderingContext2D;
 
     constructor() {
@@ -24,10 +32,99 @@ class Canvas {
         this.context = context;
     }
 
-    public drawShape(shape: Path2D, backgroundColor: Colors) {
-        this.context.fillStyle = String(backgroundColor);
-        this.context.fill(shape);
+    public addShape(shape: Shape) {
+        allShapes.push(shape);
+    }
+
+    public drawShape(shape: Shape) {
+        if(shape instanceof Rect) {
+            this.context.fillStyle = String(shape.color);
+            this.context.fillRect(shape.posX, shape.posY, shape.width, shape.height);  
+        }
+    }
+}
+
+class Shape {
+    public posX:   number;
+    public posY:   number;
+    public width:  number;
+    public height: number;
+
+    public movable:       boolean;
+    public clickdownable: boolean;
+    public dblclickable:  boolean;
+
+    public path2d: Path2D;
+}
+
+class Rect implements Shape {
+    public width:  number = 100;
+    public posY:   number = 100;
+    public posX:   number = 100;
+    public height: number = 100;
+
+    public color: Colors = Colors.MAIN;
+
+    public movable:       boolean = true;
+    public clickdownable: boolean = true;
+    public dblclickable:  boolean = true;
+
+    public path2d: Path2D = new Path2D;
+
+    constructor(posX: number, posY: number, width: number, height: number, color: Colors) {
+        this.posX   = posX;
+        this.posY   = posY;
+        this.width  = width;
+        this.height = height;
+        this.color  = color;
+
+        this.path2d.rect(this.posX, this.posY, this.width, this.height);
     }
 }
 
 const canvas: Canvas = new Canvas();
+
+canvas.canvas.addEventListener('mouseup', function(ev: MouseEvent) {
+    mouseIsDown = false;
+    selectedShape = null;
+})
+
+canvas.canvas.addEventListener('mousedown', function(ev: MouseEvent) {
+    allShapes.forEach(shape => {
+        if(canvas.context.isPointInPath(shape.path2d, ev.offsetX, ev.offsetY) && shape.clickdownable) {
+            mouseIsDown = true;
+            selectedShape = shape;
+
+            diffMouseX = ev.offsetX - shape.posX;
+            diffMouseY = ev.offsetY - shape.posY;
+        }
+    })
+})
+
+canvas.canvas.addEventListener('mousemove', function(ev: MouseEvent) {
+    if(mouseIsDown) {
+        allShapes[allShapes.indexOf(selectedShape)].posX = ev.offsetX - diffMouseX;
+        allShapes[allShapes.indexOf(selectedShape)].posY = ev.offsetY - diffMouseY;
+        
+        allShapes[allShapes.indexOf(selectedShape)].path2d = new Path2D();
+        
+        allShapes[allShapes.indexOf(selectedShape)].path2d.rect(
+            allShapes[allShapes.indexOf(selectedShape)].posX,
+            allShapes[allShapes.indexOf(selectedShape)].posY,
+            allShapes[allShapes.indexOf(selectedShape)].width,
+            allShapes[allShapes.indexOf(selectedShape)].height
+        )        
+    }
+})
+
+function renderCanvas() {
+    canvas.context.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
+
+    allShapes.forEach(shape => {
+        canvas.drawShape(shape);
+    })
+
+    window.requestAnimationFrame(renderCanvas);
+}
+
+window.requestAnimationFrame(renderCanvas);
